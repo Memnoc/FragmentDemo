0b816709e8ef949851b8b8638b182e5efe109d88
Memnoc <smartdroidesign@gmail.com>
2018-01-10 20:18:31 +0000

    - implemented the logic for passing data back and forth   - commented all the code

--- app/src/main/java/com/smartdroidesign/fragmentdemo/BlankFragment.java
+++ app/src/main/java/com/smartdroidesign/fragmentdemo/BlankFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -20,12 +22,12 @@ import android.view.ViewGroup;
 public class BlankFragment extends Fragment {
 //  6 - We delete one variable as we only need to pass one String of text
     private static final String TEXT = "text";
-
 //  7 - This will receive and contain the text we are passing from MainActivity.
     private String mText;
-
-
     private OnFragmentInteractionListener mListener;
+//  12 - Time to initialize the editText and Button we have created for the fragment.
+    private EditText editTextFragment;
+    private Button buttonFragment;
 
     public BlankFragment() {
         // Required empty public constructor
@@ -54,14 +56,35 @@ public class BlankFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+//      13 - Time to initialize the views in point 12. Put the inflater.inflate inside a view variable so you call findViewById on it and get our editText and Button.
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        buttonFragment = view.findViewById(R.id.fragmentBtn);
+        editTextFragment = view.findViewById(R.id.editText_fragment);
+
+//      14 - When our fragment opens, we want to get the text passed onto mText (point 7-8-9) and add it to editTextFragment.
+        editTextFragment.setText(mText);
+        editTextFragment.requestFocus(); // 15 - This is to make sure the fragment is automatically focused at opening.
+
+//      16 - Next, we need an onClickListener for the button. In this part we send back the content of editText and fragment to the MainActivity.
+        buttonFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sendBackText = editTextFragment.getText().toString();
+                sendBack(sendBackText); // 17 - In order to send back the text to the MainActivity, we use a method, but most importantly, we need an interface (point 18).
+
+            }
+        });
+
+
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+//    18 - We can use the dummy method provided by Android as our sendBack method. So change onButtonPressed to sendBack(); and the argument to String sendBackText.
+    public void sendBack(String sendBackText) {
+        if (mListener != null) { // 18.1 this is a check to see if our MainActivity, represented here by mListener is not null.
+            mListener.onFragmentInteraction(sendBackText); // 18.2 If not null, this method is called, which is the INTERFACE (point 19).
         }
     }
 
@@ -94,6 +117,15 @@ public class BlankFragment extends Fragment {
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onFragmentInteraction(String sendBackText); // 19 - This is the interface called in point 18.
+                                                        // Change the argument to String sendBackText, and do the same in point 18.2 on mListener.onFragmentInteraction(uri)
     }
 }
+
+
+/*The sum up is*/
+
+/*Click the Button (16)
+* SendBack gets called(17) and passes the content of editTextFragment
+* sendBack method(18) calls onFragmentInteraction on our mListener, which our MainActivity, passes the sendBackText to it(18.2)
+* This all goes back to our MainActivity which is going to implement the onFragmentInteraction method*/
--- app/src/main/java/com/smartdroidesign/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/smartdroidesign/fragmentdemo/MainActivity.java
@@ -1,13 +1,16 @@
 package com.smartdroidesign.fragmentdemo;
 
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+// 20 - Extending the OnFragmentInteractionListener method from point 19
+public class MainActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener {
 
 //    1 - create variables for views.
 
@@ -39,7 +42,26 @@ public class MainActivity extends AppCompatActivity {
 
 //    4 - Creating the method openFragment.
 //        Since we want to pass text to our fragment, the method takes one parameter String text.
+//    11 - Time to open the fragment now using this method
     public void openFragment(String text){
+        BlankFragment fragment = BlankFragment.newInstance(text); // 11.1 Creating fragment instance. The "text" within this newInstance object gets passed to BlankFragment.
+        FragmentManager fragmentManager = getSupportFragmentManager(); // 11.2 Invoking FragmentManager
+        FragmentTransaction transaction = fragmentManager.beginTransaction(); // 11.3 Beginning the transaction on the fragmentManager
+        transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_right); // 11.4 This line adds the animation to the fragment and to the button(this is why is repeated twice).
+        transaction.addToBackStack(null); // 11.4 This method specifies the when you click the back button, only the fragment gets closed, NOT the whole activity.
+        transaction.add(R.id.fragment_container, fragment, "BLANK_FRAGMENT").commit(); // 11.5 Here we add the fragment_container(ActivityMain.xml), the fragment object we have initialized, and we add a TAG to the fragment so it can be found later on.
+
+    }
+
+
+// 21 - ctrl+o and we can override the onFragmentInteraction method.
+// Our sendBackText is provided, and we cna manipulate it as we please.
+// 21.1 - We take our editText and we set it to sendBackText
+// 21.2 - We also call onBackPressed so that our fragment gets automatically closed
+    @Override
+    public void onFragmentInteraction(String sendBackText) {
+        editText.setText(sendBackText);
+        onBackPressed();
 
     }
 }
